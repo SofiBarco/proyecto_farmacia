b4e4545160820648967b415c4d68869240cc7d79
SofiBarco <sofib.62@gmail.com>
2022-09-17 20:00:18 -0300

    Entrega Proyecto Final JS

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ const searchBar = document.querySelector('#searchBar')
 const searchButton = document.querySelector('#searchButton')
 const cardProductos = document.querySelector('.cardProductos')
 const btnQuitar = document.querySelector('.btnQuitar')
+const productoAgregado = document.querySelector('.carrito')
+const totalCarrito = document.querySelector ('.total')
+const btnfinalizar = document.querySelector ('.btnfinalizar')
 
 let carritoDeCompras = []
 
@@ -40,29 +43,64 @@ const renderizarDatosProductos = (e) => {
     <img class="cardImg" src="${productoElegido.img}" class="card-img-top" alt="${productoElegido.nombre}" style="width:80px;height: 80px;">
     <h5 class="card-name">${productoElegido.nombre}</h5>
     <p class="card-precio"> $ ${productoElegido.precio}</p>
-    <button class="btnQuitar btn-primary">Quitar del carrito</button> 
-    `
+   ` //<button class="btnQuitar btn-primary">Quitar del carrito</button> 
+    
     nuevoProducto.className = 'cardProductos'
     cardProductos.append(nuevoProducto)
 
-    
-    const sumarTotal = carritoDeCompras.reduce ( (acc, precioProducto) =>{
-      return acc = acc + precioProducto 
-    },)  
-
-    console.log(sumarTotal)
 }
 
-
-
-
 btnAgregar.forEach((button) => {
-    button.addEventListener('click', renderizarDatosProductos) 
-    //button.addEventListener('clik', )
+    button.addEventListener('click', renderizarDatosProductos)
     
 })
 
+function renderizarCarrito() {
+    // Vaciamos todo el html
+    productoAgregado.innerHTML = ''
+
+    function suma(a, b) {
+        return parseInt (a) + parseInt (b);
+      }
+         
+      const sumaTotal = carritoDeCompras.reduce(suma);
+      
+        const miCarrito= document.createElement('div')
+        miCarrito.className = 'carrito'
+        miCarrito.innerHTML = `
+        <h1 class="Card-name">Total</h1>
+            <p class="card-name"> $ ${parseInt(sumaTotal)}</p>
+            <button class="btnfinalizar btn-primary">Finalizar Compra</button>
+            <button class="btnQuitar btn-primary">Vaciar carrito</button>
+        `
     
+    productoAgregado.appendChild(miCarrito)
+   
+}
+
+btnAgregar.forEach((button) => {
+    button.addEventListener('click', renderizarCarrito )
+    
+})
+
+const vaciarCarrito = () => {
+    carritoDeCompras = []
+    guardarEnStorage('carritoDeCompras', carritoDeCompras)
+    nuevoProducto.innerHTML = ''
+}
+
+
+btnfinalizar.addEventListener('click', () => {
+    Swal.fire({
+        position: 'top-end',
+        icon: 'success',
+        title: 'Tu compra ha finalizado!',
+        showConfirmButton: false,
+        timer: 1500
+      })
+   }
+   )
+     
 
 const getTodoslosproductos = async () => {
     const response = await fetch('../jsondata/productos.json')
@@ -79,18 +117,8 @@ const buscarProducto = () => {
     getTodoslosproductos(arrayResultados)
 }
 
+btnQuitar.addEventListener('click', vaciarCarrito)
 
 searchButton.addEventListener('click', buscarProducto)
 searchBar.addEventListener('input', buscarProducto)
 
-
-
-
-
-/*
-<img class="cardImg" src=" " class="card-img-top" alt="..." style="width:80px;height: 80px;">
-    <h5 class="card-name">Producto seleccionado</h5>
-    <p class="card-precio">Precio</p>
-    <button class="btnQuitar btn-primary">Quitar del carrito</button> 
-        
-*/
